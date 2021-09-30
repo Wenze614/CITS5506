a54f9eaf2f8d93f2f3f44e16b2b4b00d93d83406
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-09-30 17:34:22 +0800

    threshold update functionality

--- smart-plant-watering/src/App.js
+++ smart-plant-watering/src/App.js
@@ -5,14 +5,19 @@ import WateringLog from './components/WateringLog/WateringLog';
 import ChangeVariables from './components/ChangeVariables/ChangeVariables';
 function App() {
   const [isAuto, setIsAuto] = useState(true)
+  const [threshold, setThreshold] = useState(45)
   const switchMode = () => {
     setIsAuto(isAuto=>{return !isAuto})
     console.log(isAuto)
   }
+  const updateThreshold = (new_threshold) =>{
+    console.log("threshold updated to:" + new_threshold)
+    setThreshold(new_threshold)
+  }
   return (
     <>
       <SwitchButton isAuto={isAuto} switchMode={switchMode}/>
-      <ChangeVariables isAuto={isAuto}/>
+      <ChangeVariables isAuto={isAuto} threshold={threshold} updateThreshold={updateThreshold}/>
       <WateringLog/>
     </>
   );
--- smart-plant-watering/src/components/ChangeVariables/ChangeVariables.jsx
+++ smart-plant-watering/src/components/ChangeVariables/ChangeVariables.jsx
@@ -1,14 +1,22 @@
 import styles from "./ChangeVariables.module.css"
 import {useState} from "react"
 const ChangeVariables = (props) => {
+    const [temp_threshold, setTemp_Threshold] = useState(props.threshold)
+    const changeTemp_Threshold = (event) =>{
+        setTemp_Threshold(event.target.value)
+    }
+    const submitHandler = (event) =>{
+        event.preventDefault();
+        props.updateThreshold(temp_threshold)
+    }
     return (
         <div className={styles.container}>
             <div className={styles.header}>
                 <h2>{props.isAuto? "Automatic Mode" : "Manual Mode"}</h2>
             </div>
             <h3>Water Moisture Threshold</h3>
-            <form>
-                <input type="number" name="moistureThreshold" className={styles.inputBox} />
+            <form onSubmit={submitHandler}>
+                <input type="number" name="moistureThreshold" value={temp_threshold} onChange={changeTemp_Threshold} className={styles.inputBox} />
                 <input type="submit" value="Update" className={styles.btn} />
             </form>
         </div>

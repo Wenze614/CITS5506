226b240aecafb85a9c3388653e90e32f87dde0e6
Wenze614 <69204434+Wenze614@users.noreply.github.com>
2021-09-30 17:52:30 +0800

    add form validation check

--- smart-plant-watering/src/components/ChangeVariables/ChangeVariables.jsx
+++ smart-plant-watering/src/components/ChangeVariables/ChangeVariables.jsx
@@ -2,12 +2,25 @@ import styles from "./ChangeVariables.module.css"
 import {useState} from "react"
 const ChangeVariables = (props) => {
     const [temp_threshold, setTemp_Threshold] = useState(props.threshold)
+    const [isValid, setIsValid] = useState(true)
     const changeTemp_Threshold = (event) =>{
+        if (event.target.value <=0 || event.target.value>60)
+        {
+            setIsValid(false)
+        }
+        else{
+            setIsValid(true)
+        }
         setTemp_Threshold(event.target.value)
+
     }
     const submitHandler = (event) =>{
         event.preventDefault();
-        props.updateThreshold(temp_threshold)
+        if(isValid){
+            props.updateThreshold(temp_threshold)
+        }else{
+            setTemp_Threshold(props.threshold)
+        }
     }
     return (
         <div className={styles.container}>
@@ -16,7 +29,10 @@ const ChangeVariables = (props) => {
             </div>
             <h3>Water Moisture Threshold</h3>
             <form onSubmit={submitHandler}>
-                <input type="number" name="moistureThreshold" value={temp_threshold} onChange={changeTemp_Threshold} className={styles.inputBox} />
+                <input type="number" name="moistureThreshold" 
+                    value={temp_threshold} 
+                    onChange={changeTemp_Threshold} 
+                    className={`${styles.inputBox} ${!isValid && styles.invalid}`}/>
                 <input type="submit" value="Update" className={styles.btn} />
             </form>
         </div>

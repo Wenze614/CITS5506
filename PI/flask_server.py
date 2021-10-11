2aed24add5c63f176ca41a14e31f3ca4bdbbc2ae
Wenze614 <mangguosiji@gmail.com>
2021-10-11 21:11:36 +0800

    add a flask server

---
+++ PI/flask_server.py
@@ -0,0 +1,34 @@
+from flask import Flask,request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World!"
+
+@app.route('/',methods=["POST"])
+def update():
+    values = request.get_json()
+    if not values:
+        response={
+            'message':'No data found'
+        }
+        return response, 400
+    required_fields = ['measurement','value']
+    if not all(field in values for field in required_fields ):
+        response ={
+            'message':'Required data is missing'
+        }
+        return response, 400
+    measurement = values['measurement']
+    value = values['value']
+    if measurement == 'moisture_threshold':
+        response = {
+            'message':f'change threshold to {value}'
+        }
+        return response, 200
+    return 
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5000)
